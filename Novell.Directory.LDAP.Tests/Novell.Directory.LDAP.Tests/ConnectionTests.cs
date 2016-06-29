01f5484d4344ed7ceb88a45d3f56423b2f71deb1
Igor Shmukler <igor.shmukler@gmail.com>
2016-06-29 19:27:43 +0300

    Tests project is added. 6 tests are added.

---
+++ Novell.Directory.LDAP.Tests/Novell.Directory.LDAP.Tests/ConnectionTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xunit;
+using Novell.Directory.Ldap;
+
+namespace Novell.Directory.LDAP.Tests
+{
+    public class ConnectionTests
+    {
+        [Fact]
+        public void Ldap_Connection_Should_Connect()
+        {
+            var ldapHost = "192.168.1.32";
+            var ldapPort = 389;
+
+            var ldap = new LdapConnection();
+            ldap.Connect(ldapHost, ldapPort);
+
+            Assert.True(ldap.Connected);
+        }
+
+        [Fact]
+        public void Ldap_Connection_Should_Not_Connect()
+        {
+            var ldapHost = "0.0.0.0";
+            var ldapPort = 389;
+
+            var ldap = new LdapConnection();
+            try
+            {
+                ldap.Connect(ldapHost, ldapPort);
+            }
+            catch (Exception){}
+           
+            Assert.False(ldap.Connected);
+        }
+
+        [Fact]
+        public void Ldap_Connection_Should_Connect_And_Disconnect()
+        {
+            var ldapHost = "192.168.1.32";
+            var ldapPort = 389;
+
+            var ldap = new LdapConnection();
+            ldap.Connect(ldapHost, ldapPort);
+
+            Assert.True(ldap.Connected);
+
+            ldap.Disconnect();
+
+            Assert.False(ldap.Connected);
+        }
+    }
+}
---
+++ Novell.Directory.LDAP.Tests/Novell.Directory.LDAP.Tests/SecurityProtocolsTests.cs
@@ -0,0 +1,49 @@
+﻿using Novell.Directory.Ldap;
+using System;
+using Xunit;
+
+namespace Novell.Directory.LDAP.Tests
+{
+    public class SecurityProtocolsTests
+    {
+        [Fact]
+        public void Ldap_Connection_Should_Start_TLS()
+        {
+            var ldapHost = "192.168.1.32";
+            var ldapPort = 389;
+
+            var ldap = new LdapConnection();
+            ldap.UserDefinedServerCertValidationDelegate += (certificate, certificateErrors) => true;
+            ldap.Connect(ldapHost, ldapPort);
+            ldap.startTLS();
+            Assert.True(ldap.TLS);
+        }
+
+        [Fact]
+        public void Ldap_Connection_Should_Start_and_Stop_TLS()
+        {
+            var ldapHost = "192.168.1.32";
+            var ldapPort = 389;
+
+            var ldap = new LdapConnection();
+            ldap.UserDefinedServerCertValidationDelegate += (certificate, certificateErrors) => true;
+            ldap.Connect(ldapHost, ldapPort);
+            ldap.startTLS();
+            ldap.stopTLS();
+
+            Assert.False(ldap.TLS);
+        }
+
+        [Fact]
+        public void Ldap_Connection_Should_Not_Start_TLS_With_Invalid_Certificate_That_Is_Processed_By_Default_Certificate_Validation_Handler()
+        {
+            var ldapHost = "192.168.1.32";
+            var ldapPort = 389;
+
+            var ldap = new LdapConnection();
+            ldap.Connect(ldapHost, ldapPort);
+
+            Assert.Throws(typeof(LdapException), () => { ldap.startTLS(); });
+        }
+    }
+}
