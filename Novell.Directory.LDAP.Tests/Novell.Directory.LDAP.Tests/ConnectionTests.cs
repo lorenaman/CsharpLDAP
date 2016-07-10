d7664778e50b4726beffba0ddbf10fb86ac9f022
Igor Shmukler <igor.shmukler@gmail.com>
2016-07-10 19:47:47 +0300

    Connection tests extended

--- Novell.Directory.LDAP.Tests/Novell.Directory.LDAP.Tests/ConnectionTests.cs
+++ Novell.Directory.LDAP.Tests/Novell.Directory.LDAP.Tests/ConnectionTests.cs
@@ -49,5 +49,64 @@ namespace Novell.Directory.LDAP.Tests
 
             Assert.False(ldap.Connected);
         }
+
+        [Fact]
+        public void Ldap_Connection_Copy_Method_Should_Return_Another_Instance_Of_Object()
+        {
+            var ldapConnection = new LdapConnection();
+            var ldapConnectionClone = ldapConnection.Clone();
+
+            Assert.NotEqual(ldapConnection, ldapConnectionClone);
+        }
+
+        [Fact]
+        public void Ldap_Connection_Should_Return_Simple_Authentication_Method()
+        {
+            var ldapHost = "192.168.1.32";
+            var ldapPort = 389;
+            var loginDN = "dc=example,dc=com";
+            var password = "";
+
+            var ldap = new LdapConnection();
+            ldap.Connect(ldapHost, ldapPort);
+            ldap.Bind(loginDN, password);
+            Assert.Equal("simple", ldap.AuthenticationMethod);
+        }
+
+        [Fact]
+        public void Ldap_Connection_Should_Return_Right_Host()
+        {
+            var ldapHost = "192.168.1.32";
+            var ldapPort = 389;
+
+            var ldap = new LdapConnection();
+            ldap.Connect(ldapHost, ldapPort);
+            Assert.Equal(ldapHost, ldap.Host);
+        }
+
+        [Fact]
+        public void Ldap_Connection_Should_Return_Right_Port()
+        {
+            var ldapHost = "192.168.1.32";
+            var ldapPort = 389;
+
+            var ldap = new LdapConnection();
+            ldap.Connect(ldapHost, ldapPort);
+            Assert.Equal(ldapPort, ldap.Port);
+        }
+
+        [Fact]
+        public void Ldap_Connection_Should_Be_Authenticated()
+        {
+            var ldapHost = "192.168.1.32";
+            var ldapPort = 389;
+            var loginDN = "cn=igor.shmukler,dc=ldap,dc=vqcomms,dc=com";
+            var password = "abc123";
+
+            var ldap = new LdapConnection();
+            ldap.Connect(ldapHost, ldapPort);
+            ldap.Bind(loginDN, password);
+            Assert.True(ldap.Bound);
+        }
     }
 }
